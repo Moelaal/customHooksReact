05b44626176e011ae94d846a0e54fd6803fea8e1
Moelaal <medo.elaal@gmail.com>
2022-03-24 15:05:04 +0200

    first commit

---
+++ src/hooks/use-counter.js
@@ -0,0 +1,21 @@
+import React,{useState,useEffect} from 'react'
+
+function useCounter(forwords = true) {
+    const [counter, setCounter] = useState(0);
+
+    useEffect(() => {
+      const interval = setInterval(() => {
+          if(forwords){
+              setCounter((prevCounter) => prevCounter + 1);
+            }else{
+                setCounter((prevCounter) => prevCounter - 1);
+            }
+      }, 1000);
+  
+      return () => clearInterval(interval);
+    }, []);
+
+    return counter;
+}
+
+export default useCounter
